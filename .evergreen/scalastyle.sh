f168bba496ddcf68310be4e36414d38d37de9b51
Ross Lawley <ross.lawley@gmail.com>
2017-01-26 13:43:16 +0000

    Run ScalaStyle for the static analysis step

--- .evergreen/scalastyle.sh
+++ .evergreen/scalastyle.sh
@@ -7,9 +7,8 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #            Main Program                  #
 ############################################
 
-echo "Compiling driver with jdk8"
+echo "Compiling and running ScalaStyle"
 
-# We always compile with the latest version of java
 export JAVA_HOME="/opt/java/jdk8"
 ./sbt -java-home $JAVA_HOME version
-./sbt -java-home $JAVA_HOME clean compile
+./sbt -java-home $JAVA_HOME clean scalastyle
