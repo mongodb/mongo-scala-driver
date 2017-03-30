0c166947e1f78d9911d73349dfcc12809d1ba902
Ross Lawley <ross.lawley@gmail.com>
2017-03-30 15:21:11 +0100

    Build: Version 2.0.0

--- project/MongoScalaBuild.scala
+++ project/MongoScalaBuild.scala
@@ -32,7 +32,7 @@ object MongoScalaBuild extends Build {
   import Dependencies._
   import Resolvers._
 
-  val baseVersion = "1.2.2"
+  val baseVersion = "2.0.0"
 
   val buildSettings = Seq(
     organization := "org.mongodb.scala",
