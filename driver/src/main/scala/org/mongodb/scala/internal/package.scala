b5dcaf815271861f3c35501ac2ff11e790dd64a9
Andrii Lashchenko <andrew.lashchenko@gmail.com>
2017-05-03 20:54:03 +0300

    Fix package of object 'internal'

--- driver/src/main/scala/org/mongodb/scala/internal/package.scala
+++ driver/src/main/scala/org/mongodb/scala/internal/package.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mongodb.scala.client
+package org.mongodb.scala
 
 /**
  * Internal companion objects for easy interaction with the Java based async library
