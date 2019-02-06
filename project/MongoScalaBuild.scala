3772713375c8a5f4460b8cf8cdbe931166b9369b
Ross Lawley <ross.lawley@gmail.com>
2019-02-06 12:20:26 +0000

    Build: Bump version 2.6.0

--- project/MongoScalaBuild.scala
+++ project/MongoScalaBuild.scala
@@ -32,7 +32,7 @@ object MongoScalaBuild extends Build {
   import Dependencies._
   import Resolvers._
 
-  val baseVersion = "2.5.1"
+  val baseVersion = "2.6.0"
 
   val buildSettings = Seq(
     organization := "org.mongodb.scala",
