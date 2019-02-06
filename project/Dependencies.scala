81688510d67812cbfdd8e6e3880d09bc8f490b88
Ross Lawley <ross.lawley@gmail.com>
2019-02-06 11:35:18 +0000

    Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,9 +19,9 @@ import sbt._
 
 object Dependencies {
   // Versions
-  val scalaVersions           = Seq("2.11.12", "2.12.7")
+  val scalaVersions           = Seq("2.11.12", "2.12.8")
   val scalaCoreVersion        = "2.12.7"
-  val mongodbDriverVersion    = "3.10.0-SNAPSHOT"
+  val mongodbDriverVersion    = "3.10.0"
 
   val scalaTestVersion        = "3.0.1"
   val scalaMockVersion        = "3.4.1"
