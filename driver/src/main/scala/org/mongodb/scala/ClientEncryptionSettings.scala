45be366c427efbf81da42317c386e224ac70dab3
Ross Lawley <ross.lawley@gmail.com>
2019-09-05 12:13:41 +0100

    Copyright header normalization

--- driver/src/main/scala/org/mongodb/scala/ClientEncryptionSettings.scala
+++ driver/src/main/scala/org/mongodb/scala/ClientEncryptionSettings.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 MongoDB, Inc.
+ * Copyright 2008-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-package org.mongodb.scala
+ */ package org.mongodb.scala
 
 import com.mongodb.{ClientEncryptionSettings => JClientEncryptionSettings}
 
--- examples/scripts/mongoimport.scala
+++ examples/scripts/mongoimport.scala
@@ -5,7 +5,7 @@ exec scala -cp "$cp" "$0" "$@"
 !#
 
 /*
- * Copyright 2015 MongoDB, Inc.
+ * Copyright 2008-present MongoDB, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
