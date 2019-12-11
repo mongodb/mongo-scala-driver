f916113d31cca6f6d993de80a8a5eb1b8d8bacaa
Ross Lawley <ross.lawley@gmail.com>
2019-12-11 16:24:36 +0000

    Build: changed baseline version to 2.9.0

--- project/MongoScalaBuild.scala
+++ project/MongoScalaBuild.scala
@@ -32,7 +32,7 @@ object MongoScalaBuild extends Build {
   import Dependencies._
   import Resolvers._
 
-  val baseVersion = "2.8.0"
+  val baseVersion = "2.9.0"
 
   val buildSettings = Seq(
     organization := "org.mongodb.scala",
